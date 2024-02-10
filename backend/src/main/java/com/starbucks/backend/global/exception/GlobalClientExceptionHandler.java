d62ded360a4ca31c1b9260c4322c06b33eae99fc
sieunnnn <wldsmtldsm65@gmail.com>
2024-02-10 20:25:25 +0900

    Feat: @Valid 로 야기 되는 예외 추가 (#3)

--- backend/src/main/java/com/starbucks/backend/global/exception/GlobalClientExceptionHandler.java
+++ backend/src/main/java/com/starbucks/backend/global/exception/GlobalClientExceptionHandler.java
@@ -4,20 +4,27 @@ import jakarta.validation.ConstraintViolationException;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalClientExceptionHandler {
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<?> handleBadRequestException () {
+    public ResponseEntity<?> handleBadRequestException() {
         ErrorResponse errorResponse = new ErrorResponse("잘못된 값을 입력 하였습니다.", "CLIENT_01");
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<?> handleConstraintViolationException () {
+    public ResponseEntity<?> handleConstraintViolationException() {
         ErrorResponse errorResponse = new ErrorResponse("유효 하지 않은 값 입니다.", "CLIENT-02");
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException() {
+        ErrorResponse errorResponse = new ErrorResponse("유효 하지 않은 값 입니다.", "CLIENT-03");
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
