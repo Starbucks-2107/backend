0edc3084f82ee2a0ee8bae6efb548e2463695f69
sieunnnn <wldsmtldsm65@gmail.com>
2024-02-10 20:28:30 +0900

    Feat: 회원가입 기능 구현 (#3)

---
+++ backend/src/main/java/com/starbucks/backend/domain/user/dto/SignUpRequest.java
@@ -0,0 +1,21 @@
+package com.starbucks.backend.domain.user.dto;
+
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignUpRequest{
+    private String email;
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,}$")
+    private String password;
+    private String nickname;
+    private String username;
+    private String phoneNumber;
+}
+
